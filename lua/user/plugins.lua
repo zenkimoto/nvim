45d9b2a3be0d240b9e1941b989f5d296fbb326bb
Alex Yip <alexyip123@gmail.com>
2022-02-02 09:52:17 -0800

    Added bufferline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -48,7 +48,11 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim"     -- Have packer manage itself
   use "nvim-lua/popup.nvim"        -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"      -- Useful lua functions used ny lots of plugins
-  
+
+  -- Bufferline
+  use "akinsho/bufferline.nvim"
+  use "moll/vim-bbye"
+
   -- Color Schemes
   use "LunarVim/Colorschemes"
   use "folke/tokyonight.nvim"
@@ -93,6 +97,9 @@ return packer.startup(function(use)
   -- nvim-tree
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
+
+  -- Git
+  use "lewis6991/gitsigns.nvim"
 
   -- Autopairs
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
