cdab38eb5f2cbe6cd2743bc0134a279942cf97f5
Alex Yip <alexyip123@gmail.com>
2022-02-02 22:10:16 -0800

    Added neoscroll

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -110,6 +110,9 @@ return packer.startup(function(use)
   -- Toggleterm
   use "akinsho/toggleterm.nvim"
 
+  -- neoscroll
+  use 'karb94/neoscroll.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
